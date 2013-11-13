f49d621932c6e8afd27cf672e851eb5bfd798ca0
Bhairav Muthyapu <bhairav@tsswireless.com>
2013-11-13 12:50:27 -0800

    Fixes for group by cited counts.

--- hadoop_beginner/src/main/java/com/tsswireless/mapreduce/patent/dataset/GroupPatentCitedByCount.java
+++ hadoop_beginner/src/main/java/com/tsswireless/mapreduce/patent/dataset/GroupPatentCitedByCount.java
@@ -2,6 +2,7 @@ package com.tsswireless.mapreduce.patent.dataset;
 
 import java.io.IOException;
 import java.util.Iterator;
+import java.util.StringTokenizer;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
@@ -14,40 +15,25 @@ import org.apache.hadoop.mapred.JobClient;
 import org.apache.hadoop.mapred.JobConf;
 import org.apache.hadoop.mapred.KeyValueTextInputFormat;
 import org.apache.hadoop.mapred.MapReduceBase;
-import org.apache.hadoop.mapred.Mapper;
 import org.apache.hadoop.mapred.OutputCollector;
 import org.apache.hadoop.mapred.Reducer;
 import org.apache.hadoop.mapred.Reporter;
 import org.apache.hadoop.mapred.TextOutputFormat;
+import org.apache.hadoop.mapred.lib.IdentityMapper;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 
 public class GroupPatentCitedByCount extends Configured implements Tool
 {
-
-	
-	public static class MapClass extends MapReduceBase implements Mapper<Text, Text, Text, Text>
-	{
-		@Override
-		public void map(Text key, Text value, OutputCollector<Text, Text> output, Reporter reporter)
-			throws IOException
-		{
-			output.collect(value, key);
-		}
-	}
-	
+			
 	public static class ReduceClass extends MapReduceBase implements Reducer<Text, Text, Text, IntWritable>
 	{
 		@Override
 		public void reduce(Text key, Iterator<Text> values, OutputCollector<Text, IntWritable> output, Reporter reporter)
 			throws IOException
-		{			
-			int count = 0;
-			while (values.hasNext())
-			{				
-				count++;
-			}
-			output.collect(key, new IntWritable(count));
+		{						
+			StringTokenizer tokenizer = new StringTokenizer(values.next().toString(), ",");									
+			output.collect(key, new IntWritable(tokenizer.countTokens()));
 		}
 		
 	}
@@ -63,23 +49,20 @@ public class GroupPatentCitedByCount extends Configured implements Tool
 		Path outPath = new Path(args[1]);
 						
 		jobConf.setJobName("GroupPatentCitedByCount");
-		jobConf.setMapperClass(MapClass.class);		
+		jobConf.setMapperClass(IdentityMapper.class);		
 		jobConf.setReducerClass(ReduceClass.class);
 				
 		jobConf.setInputFormat(KeyValueTextInputFormat.class);
 		jobConf.setOutputFormat(TextOutputFormat.class);
 		jobConf.setOutputKeyClass(Text.class);
 		jobConf.setOutputValueClass(Text.class);
-		jobConf.set("key.value.separator.in.input.line", ",");
-		
+				
 		FileInputFormat.addInputPath(jobConf, inPath);
 		FileOutputFormat.setOutputPath(jobConf, outPath);
 		
 		JobClient.runJob(jobConf);
 		
-		return 0;
-		
-		// System.exit(job.waitForCompletion(true) ? 0 : 1);
+		return 0;				
 	}
 	
 	public static void main(String[] args)
